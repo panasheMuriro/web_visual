c3587775fb5eacf8de40cd5c33569cb932f07c73
murirop <panashemuriro2021@gmail.com>
2025-01-15 18:17:55 -0600

    zdog gettting started

--- zdog/intro/index.js
+++ zdog/intro/index.js
@@ -1,6 +1,10 @@
 let illo = new Zdog.Illustration({
     // set canvas with selector
+   
     element: '.zdog-canvas',
+    zoom: 1,
+    dragRotate: true,
+    
   });
   
   // add circle
@@ -9,7 +13,32 @@ let illo = new Zdog.Illustration({
     diameter: 80,
     stroke: 20,
     color: '#636',
+    dragRotate: true,
   });
+
+
+
+  function animate() {
+    // rotate illo each frame
+    illo.rotate.y += 0.01;
+    illo.updateRenderGraph();
+    // animate next frame
+    requestAnimationFrame( animate );
+  }
+  // start animation
+  animate();
+
+
+
+  let pentagon = new Zdog.Polygon({
+    addTo: illo,
+    radius: 100,
+    sides: 6,
+    stroke: 20,
+    color: '#EA0',
+  });
+
+  
   
   // update & render
   illo.updateRenderGraph();
